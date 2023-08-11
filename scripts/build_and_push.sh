d6ce47252ee170eaa10853ccd0db1f4504741b51
bowan <bowan@microsoft.com>
2023-08-11 12:46:21 +0800

    change profile from "docker" to "kubernetes" and app scripts.

---
+++ scripts/build_and_push.sh
@@ -0,0 +1,7 @@
+./mvnw clean install -DskipTests -P buildDocker
+docker push bowan.azurecr.io/spring-petclinic-api-gateway:latest
+docker push bowan.azurecr.io/spring-petclinic-config-server:latest
+docker push bowan.azurecr.io/spring-petclinic-visits-service:latest
+docker push bowan.azurecr.io/spring-petclinic-vets-service:latest
+docker push bowan.azurecr.io/spring-petclinic-customers-service:latest
+docker push bowan.azurecr.io/spring-petclinic-admin-server:latest
---
+++ scripts/deploy.sh
@@ -0,0 +1,2 @@
+kubectl delete deploy api-gateway config-server customers-service vets-service visits-service -n spring-petclinic
+kubectl apply -f kubernetes/
